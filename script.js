927bf8d7a17b05884f175f7f8f238e456c219378
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-16 19:10:44 -0700

    local military time working!

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  var now = dayjs(); 
+  var now = dayjs().format('HH'); // Perfect!
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
