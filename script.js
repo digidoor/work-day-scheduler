c3675af3a56fd866b40701754efcbfc249de6cec
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-16 19:03:39 -0700

    test date and removed preset time classes

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-   
+  var now = dayjs(); 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -28,6 +28,8 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
   //! testing by alan
+  
+  $("#currentHour").text(now);
   $("#testDiv").text("please work");
   console.log("hello");
 });
